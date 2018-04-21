9d1fa7fc698297272769cebfb307d6b45f958fc2
VelislavaRogleva <v.rogleva@gmail.com>
2018-04-21 17:28:50 +0300

    add logging interceptor

---
+++ server/src/main/java/app/urbanist/interceptors/RequestInterceptor.java
@@ -0,0 +1,23 @@
+package app.urbanist.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestInterceptor extends HandlerInterceptorAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("Requested: " + request.getMethod() + " :" + request.getRequestURI());
+        return super.preHandle(request, response, handler);
+    }
+
+}
